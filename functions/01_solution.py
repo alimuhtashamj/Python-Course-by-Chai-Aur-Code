2c7162a02388ff630a47ed4b7b53adf6c2ac1d23
Ali Muhtasham Javed <alimuhtashamjawi@gmail.com>
2026-01-01 10:45:46 +0500

    committed

---
+++ functions/01_solution.py
@@ -0,0 +1,4 @@
+def squared(num):
+    return (num ** 2)
+
+print(squared(2))
---
+++ functions/02_solution.py
@@ -0,0 +1,5 @@
+# Create a function that takes two numbers as parameters and returns their sum.
+def add(num1, num2):
+    return num1+ num2
+result = add(2,1)
+print(result)
